68d42a9682568cdb87a57ad36d215579d256a4b9
Maxim <77147114+Rijiy9013@users.noreply.github.com>
2024-01-03 14:54:48 +0400

    task5 try1

---
+++ task5/src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h2>Админка фильмотеки</h2>
+            <label className="label">Зорин Максим 6409-010302D ПМИ</label>
+        </header>
+    );
+}
+
+export default Header;
